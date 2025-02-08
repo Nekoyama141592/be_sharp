dea7a48122eb1a83ddce428927fedb66e0a8a2ce
Nekoyama141592 <mtaky0904@gmail.com>
2025-02-08 12:32:03 +0900

    add: スタイルUICore

---
+++ lib/ui_core/style_ui_core.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class StyleUICore {
+  static double _bodyLargeFontSize(BuildContext context) {
+    return Theme.of(context).textTheme.bodyLarge?.fontSize ?? 14.0;
+  }
+
+  static int _listTileLetterCount(BuildContext context) {
+    const widthRate = 0.6;
+    final width = MediaQuery.of(context).size.width * widthRate;
+    final result = width ~/ _bodyLargeFontSize(context);
+    return result;
+  }
+
+  static String limitListTileText(BuildContext context, String text) {
+    final limit = _listTileLetterCount(context);
+    if (text.length > limit) {
+      return '${text.substring(0, limit)}...';
+    }
+    return text;
+  }
+}
