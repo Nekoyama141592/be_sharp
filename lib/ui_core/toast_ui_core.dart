8fed5328d67727cb04e00f297a37152e2662a282
Nekoyama141592 <mtaky0904@gmail.com>
2025-04-15 13:38:01 +0900

    change: toast

--- lib/ui_core/toast_ui_core.dart
+++ lib/ui_core/toast_ui_core.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart' as fluttertoast;
+import 'package:fluttertoast/fluttertoast.dart' as ft;
 import 'package:get/get.dart';
 
 class ToastUICore {
@@ -12,12 +12,14 @@ class ToastUICore {
   }
 
   static Future<void> showFlutterToast(String msg) async {
-    await fluttertoast.Fluttertoast.showToast(
+    await ft.Fluttertoast.showToast(
+      gravity: ft.ToastGravity.TOP,
         msg: msg, timeInSecForIosWeb: timeInSecForIosWeb);
   }
 
   static Future<void> showErrorFlutterToast(String msg) async {
-    await fluttertoast.Fluttertoast.showToast(
+    await ft.Fluttertoast.showToast(
+      gravity: ft.ToastGravity.TOP,
         msg: msg,
         timeInSecForIosWeb: timeInSecForIosWeb,
         backgroundColor: Colors.red);
