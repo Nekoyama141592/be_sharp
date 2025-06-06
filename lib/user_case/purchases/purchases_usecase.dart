44b63760b0e281ace7b4bc99760d55a75557b05b
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-06 22:33:46 +0900

    保存

--- lib/user_case/purchases/purchases_usecase.dart
+++ lib/user_case/purchases/purchases_usecase.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:be_sharp/model/firestore_model/verified_purchase/verified_purchase.dart';
 import 'package:be_sharp/provider/repository/cloud_functions/cloud_functions_repository_provider.dart';
 import 'package:be_sharp/repository/cloud_functions_repository.dart';
-import 'package:be_sharp/repository/result.dart';
+import 'package:be_sharp/repository/result/result.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
